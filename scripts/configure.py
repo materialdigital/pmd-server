5f9964ce6119fb776c14cf18c1355a371ff2b450
Joerg Schaarschmidt <joerg.schaarschmidt@kit.edu>
2021-06-08 14:37:58 +0200

    Update configure script to handle a static.json as well

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,17 +1,33 @@
 import json
+from os.path import isfile
 
-with open('config.json') as fh:
-  config = json.load(fh)
-shared_vars = config.pop('shared', dict())
+def load_config(file_name):
+    if isfile(file_name):
+        with open(file_name) as fh:
+            return json.load(fh)
+    else:
+        return dict()
+
 
 def get_value(value):
-  if value.startswith("shared:"):
-    return shared_vars.get(value[7:], value)
-  else:
-    return value
+    if value.startswith("shared:"):
+        return shared_vars.get(value[7:], value)
+    else:
+        return value
 
-for env_file, entry in config.items():
-  with open(env_file, 'w') as fh:
-    lines = [ "{}={}\n".format(key, get_value(val)) for key, val in entry.items()]
-    print("### Writing {}...". format(env_file))
-    fh.writelines(lines)
+
+if __name__ == '__main__':
+    config = load_config('static.json')
+
+    for env_file, entry in load_config('config.json').items():
+        if env_file in config:
+            config[env_file].update(entry)
+        else:
+            config[env_file] = entry
+
+    shared_vars = config.pop('shared', dict())
+    for env_file, entry in config.items():
+        with open(env_file, 'w') as fh:
+            lines = [ "{}={}\n".format(key, get_value(val)) for key, val in entry.items()]
+            print("### Writing {}...". format(env_file))
+            fh.writelines(lines)
